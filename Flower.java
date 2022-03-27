2cf73232dc8f70aafdd286274f15dc90a9d1fb9f
evelynvsx <102490601+evelynvsx@users.noreply.github.com>
2022-03-28 12:06:51 +1300

    Draw in the stem and the flower

--- Flower.java
+++ Flower.java
@@ -63,4 +63,20 @@ public class Flower
     public void setBottom() {
         this.bottom = this.flowerY + this.flowerHeight;
     }
+    
+    /**
+     * Draw the flower on screen
+     */
+    public void draw() {
+        //draw stem
+        UI.setColor(Color.green);      //set color of the stem
+        UI.setLineWidth(2);            //set width of the stem
+        UI.drawLine(flowerX, flowerY, flowerX, bottom); //draw in stem
+        
+        //draw flower
+        UI.setColor(this.color);                        //set flower color
+        UI.fillOval(left, top, flowerSize, flowerSize); //draw flower
+        UI.sleep(500);                                  //wait
+        
+    }
 }
